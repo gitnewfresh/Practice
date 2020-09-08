e6aad65c6c029ca8724da02ca425094122a34b6e
gitnewfresh <1150232696@qq.com>
2020-09-08 17:13:28 +0800

    .

---
+++ WebAPIdemoone.BLL/StudentBLL.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebAPIdemoone.BLL
+{
+    public class StudentBLL
+    {
+
+        /// <summary>
+        /// 添加学生
+        /// </summary>
+        public void AddStudent()
+        {
+
+
+        }
+    }
+}
---
+++ WebAPIdemoone.DAL/StudentDAL.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebAPIdemoone.Model.Models;
+
+namespace WebAPIdemoone.DAL
+{
+    public class StudentDAL:DbContext<Student>
+    {
+    }
+}
